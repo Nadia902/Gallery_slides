a48db4c6b55482342e8d2e73495d2634cd587341
Nadia <85528630+Nadia902@users.noreply.github.com>
2023-03-16 18:51:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let slides = document.querySelectorAll(".slide");
+
+for(let i = 0; i< slides.length; i++) {
+    slides[i].addEventListener('click', () => {
+        clearActiveClasses()
+        slides[i].classList.add('active');
+    });
+}
+
+function clearActiveClasses(){
+    for(let i = 0; i< slides.length; i++) {
+        slides[i].classList.remove('active');
+    }
+}
